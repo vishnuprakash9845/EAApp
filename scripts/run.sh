16a415ee2d90fbb85e5a8efdf504daf05f8cc0a7
vishnuprakash9845 <vishnuprakash9845@gmail.com>
2022-02-05 14:36:28 +0000

    final

---
+++ scripts/run.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+set -env
+set -x
+
+project="e2etest"
+
+cd "$(dirname "${0}")/.."
+
+export COMPOSE_HTTP_TIMEOUT=200
+
+docker-compose -p "$project" build
+
+docker-compose -p "$project" up -d ea_api ea_webapp db selenium-hub node-docker
+
+docker-compose -p "$project" up --no-deps ea_test
+
+exit_code=$(docker inspect ea_test -f '{{ .State.ExitCode }}')
+
+if [ $exit_code -eq 0 ]; then
+    exit $exit_code
+else
+    echo "Test failed"
+fi
